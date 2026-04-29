2c62b1e593acf3afc55d8d80fc83cd594a6c95ce
mjstrus <mjstrus@gmail.com>
2026-04-29 11:18:57 +0200

    Create 2_Szablony.py

---
+++ pages/2_Szablony.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+from auth import ABACUS_NAVY, check_password, logout_button
+
+st.set_page_config(page_title="Szablony | Abacus", layout="wide")
+check_password()
+
+st.markdown(
+    f"<h1 style='color:{ABACUS_NAVY};font-weight:700'>Szablony</h1>",
+    unsafe_allow_html=True,
+)
+st.info("Moduł w budowie")
+logout_button()
